84997f51d2bcbc92862678df04e70309792bdf58
DanaAlmalki <danaalmalkexvii@gmail.com>
2024-11-03 11:56:20 +0300

    set up + fetching artworks and sending to products component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import NavBar from "./components/navbar/NavBar.js";
+import Products from "./components/products/Products.js";
 
 function App() {
-  return <div className="App"></div>;
+  const productUrl = "http://localhost:5125/api/v1/artworks";
+
+  const [productResponse, setProductResponse] = useState({
+    artworks: [],
+    totalCount: 0,
+  });
+
+  function getData() {
+    axios.get(productUrl).then((response) => {
+      setProductResponse(response.data);
+      //console.log(response);
+      //console.log(response.data);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Products response={productResponse} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
-export default function Products() {
+import "./Products.css";
+
+export default function Products(prop) {
+  const { response } = prop;
+  const { artworks, totalCount } = response;
+  console.log(artworks);
+  console.log(totalCount);
+
   return (
-    <div>Products</div>
-  )
+    <div>
+      <p>Products</p>
+    </div>
+  );
 }
